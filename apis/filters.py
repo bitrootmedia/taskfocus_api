1283d2562862ce31bf66e6291c54c8c8c5196738
Jakub Bernatowicz <info@bitrootmedia.com>
2023-08-21 11:18:55 +0100

    Add tasks date filter using _before _after - DateFromToRangeFilter

--- apis/filters.py
+++ apis/filters.py
@@ -26,6 +26,7 @@ class ProjectFilter(filters.FilterSet):
 
 class TaskFilter(filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr="icontains")
+    created_at = filters.DateFromToRangeFilter()
 
     class Meta:
         model = Task
