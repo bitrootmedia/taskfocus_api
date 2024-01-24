793b857f0b4dd6c6ab4ba26a5c7a328f88ab79bf
Karol Alvarado <k.alv@protonmail.com>
2024-01-24 14:27:39 +0100

    Change time calculation method and format

--- core/templatetags/time_from_seconds.py
+++ core/templatetags/time_from_seconds.py
@@ -1,11 +1,11 @@
-from time import gmtime, strftime
-
 from django import template
 
 register = template.Library()
 
 
 @register.filter
-def time_from_seconds(value):
-    # NOTE: The following resets if it goes over 23:59:59
-    return strftime("%H:%M:%S", gmtime(value))
+def time_from_seconds(seconds_value):
+    minutes, seconds = divmod(seconds_value, 60)
+    hours, minutes = divmod(minutes, 60)
+    
+    return f"{hours}h {minutes}m {seconds}s"
