5d3f192d0513011ac3aa3145f8518b3f511a5b7e
Karol Alvarado <k.alv@protonmail.com>
2024-02-28 16:23:56 +0100

    Adjust DEFAULT_AUTHENTICATION_CLASSES

--- pma/settings.py
+++ pma/settings.py
@@ -108,8 +108,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "apis.paginations.CustomPagination",
