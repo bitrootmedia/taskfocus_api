a255da7f5a99d3421fe5eddd97d0d59517a5b14b
koressi <k.alv@protonmail.com>
2024-08-31 12:57:46 +0200

    Update filtering by blocks

--- apis/filters.py
+++ apis/filters.py
@@ -54,9 +54,7 @@ class TaskFilter(filters.FilterSet):
             Q(title__icontains=value)
             | Q(description__icontains=value)
             | Q(tag__icontains=value)
-            # JSONFiled filters
-            | Q(blocks__content__markdown__icontains=value)  # markdown
-            | Q(blocks__content__title__icontains=value)  # checklist
+            | Q(blocks__content__icontains=value)  # content JSONField
         )
 
 
