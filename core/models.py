f2efc2eddbf1997bff45121bcc0267078c5f0290
koressi <k.alv@protonmail.com>
2024-09-15 23:01:55 +0200

    Add id filed to Pin

--- core/models.py
+++ core/models.py
@@ -181,6 +181,7 @@ class TaskBlock(models.Model):
 
 
 class Pin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
