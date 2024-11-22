466f9f9e9ba4c4588c33f9820f6b8018230b36cf
koressi <k.alv@protonmail.com>
2024-11-22 08:48:19 +0100

    Adjust test to check for PermissionDenied

--- apis/tests/test_boards.py
+++ apis/tests/test_boards.py
@@ -168,10 +168,10 @@ class BoardTest(APITestCase):
         response = self.client.delete(
             reverse("board_detail", kwargs={"pk": self.board.id})
         )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(
             "Cannot remove a board that contains items",
-            response.json()["error"],
+            response.json()["detail"],
         )
 
     def test_board_detail_delete(self):
