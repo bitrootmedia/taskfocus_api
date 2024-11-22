47d7d0e4f295e1b221ab7efd8762aaf9a99bb7fc
koressi <k.alv@protonmail.com>
2024-11-22 08:48:54 +0100

    Test deleting card that contains items

--- apis/tests/test_boards.py
+++ apis/tests/test_boards.py
@@ -307,7 +307,21 @@ class BoardTest(APITestCase):
         self.card.refresh_from_db()
         self.assertEqual(self.card.name, "New Name")
 
+    def test_card_delete_not_empty(self):
+        self.client.force_login(self.user)
+        response = self.client.delete(
+            reverse("card_detail", kwargs={"pk": self.card.id})
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            "Cannot remove a card that contains items",
+            response.json()["detail"],
+        )
+
     def test_card_delete(self):
+        # Delete all items from the card
+        CardItem.objects.filter(card=self.card).delete()
+
         self.client.force_login(self.user)
         response = self.client.delete(
             reverse("card_detail", kwargs={"pk": self.card.id})
